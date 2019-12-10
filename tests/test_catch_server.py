4540c6285f3aaecc270192ebacb933441f1b1424
Jan Bednařík <jan.bednarik@gmail.com>
2019-12-10 14:11:52 +0100

    Test for multiple requests

--- tests/test_catch_server.py
+++ tests/test_catch_server.py
@@ -121,3 +121,32 @@ def test_catch_server__delete(testdir):
 
     result.stdout.fnmatch_lines(["*::test_delete PASSED*"])
     assert result.ret == 0
+
+
+def test_catch_server__multiple_requests(testdir):
+    testdir.makepyfile(
+        """
+        import urllib.request
+
+        def test_multiple_requests(catch_server):
+            for n, method in enumerate(["PUT", "POST", "PATCH"]):
+                url = "http://{cs.host}:{cs.port}/req_{n}".format(cs=catch_server, n=n)
+                data = "{} {}".format(n, method).encode("utf-8")
+                request = urllib.request.Request(url, method=method, data=data)
+
+                with urllib.request.urlopen(request) as response:
+                    assert response.status == 200
+                    assert response.read() == b"OK"
+
+            assert catch_server.requests == [
+                {"method": "PUT", "path": "/req_0", "data": b"0 PUT"},
+                {"method": "POST", "path": "/req_1", "data": b"1 POST"},
+                {"method": "PATCH", "path": "/req_2", "data": b"2 PATCH"},
+            ]
+        """
+    )
+
+    result = testdir.runpytest("-v")
+
+    result.stdout.fnmatch_lines(["*::test_multiple_requests PASSED*"])
+    assert result.ret == 0
